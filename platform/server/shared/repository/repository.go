2b38b960ab1b27ffe6837b2fdf781c97dd5e32ba
Nihilism <843434160@qq.com>
2023-08-08 17:07:04 +0800

    add repository logic

--- platform/server/shared/repository/repository.go
+++ platform/server/shared/repository/repository.go
@@ -16,11 +16,155 @@
 
 package repository
 
+import (
+	"context"
+	"github.com/google/go-github/v53/github"
+	"github.com/xanzy/go-gitlab"
+	"io/ioutil"
+)
+
 type IRepository interface {
+	GetRepoInfo() (*Repository, error)
+	GetFile(filePath, ref string) (*File, error)
+	PushFilesToRepository(files map[string][]byte, branch, commitMessage string) error
 }
 
 type GitHubApi struct {
+	Client   github.Client
+	Owner    string
+	RepoName string
 }
 
 type GitLabApi struct {
+	Client    gitlab.Client
+	Owner     string
+	ProjectID int
+}
+
+type File struct {
+	Name    string
+	Content []byte
+}
+
+type Repository struct {
+	Name string
+	URL  string
+}
+
+func (g *GitHubApi) GetRepoInfo() (*Repository, error) {
+	repo, _, err := g.Client.Repositories.Get(context.Background(), g.Owner, g.RepoName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Repository{
+		Name: repo.GetName(),
+		URL:  repo.GetURL(),
+	}, nil
+}
+
+func (g *GitHubApi) GetFile(filePath, ref string) (*File, error) {
+	fileContent, _, err := g.Client.Repositories.DownloadContents(context.Background(), g.Owner, g.RepoName, filePath, &github.RepositoryContentGetOptions{Ref: ref})
+	if err != nil {
+		return nil, err
+	}
+	defer fileContent.Close()
+
+	content, err := ioutil.ReadAll(fileContent)
+	if err != nil {
+		return nil, err
+	}
+
+	return &File{
+		Name:    filePath,
+		Content: content,
+	}, nil
+}
+
+func (g *GitHubApi) PushFilesToRepository(files map[string][]byte, branch, commitMessage string) error {
+	// 获取默认分支的引用
+	ref, _, err := g.Client.Git.GetRef(context.Background(), g.Owner, g.RepoName, "refs/heads/"+branch)
+	if err != nil {
+		return err
+	}
+
+	// 为要推送的文件创建新的树（Tree）对象
+	var treeEntries []*github.TreeEntry
+	for filePath, content := range files {
+		treeEntries = append(treeEntries, &github.TreeEntry{
+			Path:    github.String(filePath),
+			Content: github.String(string(content)),
+			Mode:    github.String("100644"),
+		})
+	}
+	newTree, _, err := g.Client.Git.CreateTree(context.Background(), g.Owner, g.RepoName, *ref.Object.SHA, treeEntries)
+	if err != nil {
+		return err
+	}
+
+	// 创建一个新的提交（Commit）对象，将新树作为其基础
+	newCommit, _, err := g.Client.Git.CreateCommit(context.Background(), g.Owner, g.RepoName, &github.Commit{
+		Message: github.String(commitMessage),
+		Tree:    newTree,
+	})
+	if err != nil {
+		return err
+	}
+
+	// 更新分支引用，将其指向新提交
+	_, _, err = g.Client.Git.UpdateRef(context.Background(), g.Owner, g.RepoName, &github.Reference{
+		Ref: github.String("refs/heads/" + branch),
+		Object: &github.GitObject{
+			SHA:  newCommit.SHA,
+			Type: github.String("commit"),
+		},
+	}, true)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (gl *GitLabApi) GetRepoInfo() (*Repository, error) {
+	proj, _, err := gl.Client.Projects.GetProject(gl.ProjectID, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Repository{
+		Name: proj.Name,
+		URL:  proj.WebURL,
+	}, nil
+}
+
+func (gl *GitLabApi) GetFile(filePath, ref string) (*File, error) {
+	fileContent, _, err := gl.Client.RepositoryFiles.GetFile(gl.ProjectID, filePath, &gitlab.GetFileOptions{Ref: &ref})
+	if err != nil {
+		return nil, err
+	}
+
+	return &File{
+		Name:    filePath,
+		Content: []byte(fileContent.Content),
+	}, nil
+}
+
+func (gl *GitLabApi) PushFilesToRepository(files map[string][]byte, branch, commitMessage string) error {
+	// Implement PushFilesToRepository for GitLab
+	for filePath, content := range files {
+		contentStr := string(content)
+		opts := &gitlab.CreateFileOptions{
+			Branch:        gitlab.String(branch),
+			CommitMessage: gitlab.String(commitMessage),
+			Content:       &contentStr,
+		}
+
+		_, _, err := gl.Client.RepositoryFiles.CreateFile(gl.ProjectID, filePath, opts)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
