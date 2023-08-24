045f770e96471eaead1563f6c01e12f5806cffdf
Nihilism <843434160@qq.com>
2023-08-24 20:44:53 +0800

    feat(platform): add sql file && fix

---
+++ platform/docs/sql/idl.sql
@@ -0,0 +1,20 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for idl
+-- ----------------------------
+DROP TABLE IF EXISTS `idl`;
+CREATE TABLE `idl` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `repository_id` bigint(20) NOT NULL COMMENT '仓库ID',
+  `main_idl_path` varchar(255) NOT NULL COMMENT '主要IDL路径',
+  `content` text NOT NULL COMMENT '内容',
+  `service_name` varchar(255) NOT NULL COMMENT '服务名称',
+  `last_sync_time` varchar(255) NOT NULL COMMENT '最后同步时间',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='IDL表';
+
+SET FOREIGN_KEY_CHECKS = 1;
