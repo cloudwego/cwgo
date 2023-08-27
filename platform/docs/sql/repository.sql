ad489d0e74d3e7c7818b609f9d86b54eea177439
Nihilism <843434160@qq.com>
2023-08-27 14:07:55 +0800

    feat(platform): fix sql file

--- platform/docs/sql/repository.sql
+++ platform/docs/sql/repository.sql
@@ -7,10 +7,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `repository`;
 CREATE TABLE `repository` (
   `id` varchar(36) NOT NULL COMMENT '仓库ID',
-  `repository_url` varchar(255) NOT NULL COMMENT '仓库URL',
+  `repository_url` varchar(65535) NOT NULL COMMENT '仓库URL',
   `last_update_time` datetime DEFAULT NULL COMMENT '最后更新时间',
   `last_sync_time` datetime DEFAULT NULL COMMENT '最后同步时间',
-  `token` varchar(255) DEFAULT '' COMMENT '令牌',
+  `token` varchar(65535) DEFAULT '' COMMENT '令牌',
   `status` varchar(20) DEFAULT 'active' COMMENT '状态',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
--- platform/docs/sql/user.sql
+++ platform/docs/sql/user.sql
@@ -10,7 +10,7 @@ CREATE TABLE `user` (
   `name` varchar(255) NOT NULL COMMENT '姓名',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `isDeleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
 
