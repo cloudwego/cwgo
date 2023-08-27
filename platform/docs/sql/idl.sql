5e12229cc176db12149d27bf1c3f7420ce294ad3
Nihilism <843434160@qq.com>
2023-08-27 13:54:16 +0800

    feat(platform): transfer timestamp to datetime

--- platform/docs/sql/idl.sql
+++ platform/docs/sql/idl.sql
@@ -12,8 +12,8 @@ CREATE TABLE `idl` (
   `content` text NOT NULL COMMENT '内容',
   `service_name` varchar(255) NOT NULL COMMENT '服务名称',
   `last_sync_time` varchar(255) NOT NULL COMMENT '最后同步时间',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='IDL表';
 
--- platform/docs/sql/template.sql
+++ platform/docs/sql/template.sql
@@ -9,8 +9,8 @@ CREATE TABLE `template` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `name` varchar(255) NOT NULL COMMENT '名称',
   `type` tinyint(4) NOT NULL COMMENT '模板类型',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='模板表';
 
