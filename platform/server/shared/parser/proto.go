de7790f2bd154435509a629a2466f56432cdcb2c
Nihilism <843434160@qq.com>
2023-10-22 01:13:09 +0800

    feat(platform): add comments

--- platform/server/shared/parser/proto.go
+++ platform/server/shared/parser/proto.go
@@ -10,27 +10,34 @@ import (
 type ProtoFile struct{}
 
 func (_ *ProtoFile) GetDependentFilePaths(mainIdlPath string) ([]string, error) {
+	// Create maps to keep track of processed and related paths
 	processedPaths := make(map[string]bool)
 	relatedPaths := make(map[string]bool)
 	var resultPaths []string
 
+	// Get the base directory of the main IDL file
 	baseDir := filepath.Dir(mainIdlPath)
 
+	// Define a function to process each file recursively
 	var processFile func(filePath string) error
 	processFile = func(filePath string) error {
+		// If the file has already been processed, skip it
 		if processedPaths[filePath] {
 			return nil
 		}
 
+		// Read the content of the Thrift file
 		thriftContent, err := ioutil.ReadFile(filePath)
 		if err != nil {
 			return err
 		}
 		regex := regexp.MustCompile(importPattern)
 
+		// Find all import statements in the Thrift file
 		matches := regex.FindAllStringSubmatch(string(thriftContent), -1)
 		var includePaths []string
 
+		// Extract the paths from the import statements
 		for _, match := range matches {
 			if len(match) >= 2 {
 				includePath := match[1]
@@ -44,8 +51,10 @@ func (_ *ProtoFile) GetDependentFilePaths(mainIdlPath string) ([]string, error) 
 			}
 		}
 
+		// Mark the current file as processed
 		processedPaths[filePath] = true
 
+		// Recursively process the included files
 		for _, includePath := range includePaths {
 			if !relatedPaths[includePath] {
 				relatedPaths[includePath] = true
@@ -60,11 +69,13 @@ func (_ *ProtoFile) GetDependentFilePaths(mainIdlPath string) ([]string, error) 
 		return nil
 	}
 
+	// Start the recursive processing with the main IDL file
 	err := processFile(mainIdlPath)
 	if err != nil {
 		return nil, err
 	}
 
+	// Calculate the relative paths to the main IDL file
 	mainIdlDir := filepath.Dir(mainIdlPath)
 	relativePaths := make([]string, len(resultPaths))
 	for i, path := range resultPaths {
--- platform/server/shared/parser/thrift.go
+++ platform/server/shared/parser/thrift.go
@@ -9,27 +9,33 @@ import (
 type ThriftFile struct{}
 
 func (_ *ThriftFile) GetDependentFilePaths(mainIdlPath string) ([]string, error) {
+	// Create maps to keep track of processed and related paths
 	processedPaths := make(map[string]bool)
 	relatedPaths := make(map[string]bool)
 	var resultPaths []string
 
+	// Define a function to process each file recursively
 	var processFile func(filePath string) error
 	processFile = func(filePath string) error {
+		// If the file has already been processed, skip it
 		if processedPaths[filePath] {
 			return nil
 		}
 
+		// Read the content of the Thrift file
 		thriftContent, err := ioutil.ReadFile(filePath)
 		if err != nil {
 			return err
 		}
 		regex := regexp.MustCompile(includePattern)
 
+		// Find all include statements in the Thrift file
 		matches := regex.FindAllStringSubmatch(string(thriftContent), -1)
 		var includePaths []string
 
 		baseDir := filepath.Dir(filePath)
 
+		// Extract the paths from the include statements
 		for _, match := range matches {
 			if len(match) >= 2 {
 				includePath := match[1]
@@ -38,8 +44,10 @@ func (_ *ThriftFile) GetDependentFilePaths(mainIdlPath string) ([]string, error)
 			}
 		}
 
+		// Mark the current file as processed
 		processedPaths[filePath] = true
 
+		// Recursively process the included files
 		for _, includePath := range includePaths {
 			if !relatedPaths[includePath] {
 				relatedPaths[includePath] = true
@@ -54,11 +62,13 @@ func (_ *ThriftFile) GetDependentFilePaths(mainIdlPath string) ([]string, error)
 		return nil
 	}
 
+	// Start the recursive processing with the main IDL file
 	err := processFile(mainIdlPath)
 	if err != nil {
 		return nil, err
 	}
 
+	// Calculate the relative paths to the main IDL file
 	mainIdlDir := filepath.Dir(mainIdlPath)
 	relativePaths := make([]string, len(resultPaths))
 	for i, path := range resultPaths {
