a80c111b57cf89e37432c912058154305ae09946
Nihilism <843434160@qq.com>
2023-11-06 15:38:00 +0800

    feat(platform): finish sync IDLs

--- platform/server/shared/dao/internal/idl/idl.go
+++ platform/server/shared/dao/internal/idl/idl.go
@@ -66,7 +66,6 @@ func (m *MysqlIDLManager) AddIDL(ctx context.Context, idlModel model.IDL) error 
 
 	mainIdlEntity := entity.MysqlIDL{
 		RepositoryID: idlModel.RepositoryId,
-		IdlType:      idlModel.IdlType,
 		IdlPath:      idlModel.MainIdlPath,
 		CommitHash:   idlModel.CommitHash,
 		ServiceName:  idlModel.ServiceName,
@@ -86,7 +85,6 @@ func (m *MysqlIDLManager) AddIDL(ctx context.Context, idlModel model.IDL) error 
 		importedIdlEntities[i] = &entity.MysqlIDL{
 			RepositoryID: idlModel.RepositoryId,
 			ParentIdlID:  mainIdlEntity.ID,
-			IdlType:      idlModel.IdlType,
 			IdlPath:      importIdl.IdlPath,
 			CommitHash:   importIdl.CommitHash,
 			ServiceName:  idlModel.ServiceName,
@@ -117,7 +115,6 @@ func (m *MysqlIDLManager) UpdateIDL(ctx context.Context, idlModel model.IDL) err
 		ID:           idlModel.Id,
 		RepositoryID: idlModel.RepositoryId,
 		ParentIdlID:  0,
-		IdlType:      idlModel.IdlType,
 		IdlPath:      idlModel.MainIdlPath,
 		CommitHash:   idlModel.CommitHash,
 		ServiceName:  idlModel.ServiceName,
@@ -136,7 +133,6 @@ func (m *MysqlIDLManager) UpdateIDL(ctx context.Context, idlModel model.IDL) err
 			importedIdlEntities[i] = &entity.MysqlIDL{
 				RepositoryID: idlModel.RepositoryId,
 				ParentIdlID:  mainIdlEntity.ID,
-				IdlType:      idlModel.IdlType,
 				IdlPath:      importIdl.IdlPath,
 				CommitHash:   importIdl.CommitHash,
 				ServiceName:  idlModel.ServiceName,
@@ -166,7 +162,6 @@ func (m *MysqlIDLManager) Sync(ctx context.Context, idlModel model.IDL) error {
 		ID:           idlModel.Id,
 		RepositoryID: idlModel.RepositoryId,
 		ParentIdlID:  0,
-		IdlType:      idlModel.IdlType,
 		IdlPath:      idlModel.MainIdlPath,
 		CommitHash:   idlModel.CommitHash,
 		ServiceName:  idlModel.ServiceName,
@@ -186,7 +181,6 @@ func (m *MysqlIDLManager) Sync(ctx context.Context, idlModel model.IDL) error {
 			importedIdlEntities[i] = &entity.MysqlIDL{
 				RepositoryID: idlModel.RepositoryId,
 				ParentIdlID:  mainIdlEntity.ID,
-				IdlType:      idlModel.IdlType,
 				IdlPath:      importIdl.IdlPath,
 				CommitHash:   importIdl.CommitHash,
 				ServiceName:  idlModel.ServiceName,
@@ -240,7 +234,6 @@ func (m *MysqlIDLManager) GetIDL(ctx context.Context, id int64) (*model.IDL, err
 	return &model.IDL{
 		Id:           mainIdlEntity.ID,
 		RepositoryId: mainIdlEntity.RepositoryID,
-		IdlType:      mainIdlEntity.IdlType,
 		MainIdlPath:  mainIdlEntity.IdlPath,
 		CommitHash:   mainIdlEntity.CommitHash,
 		ImportIdls:   importIdlModels,
@@ -288,7 +281,6 @@ func (m *MysqlIDLManager) GetIDLList(ctx context.Context, page, limit, order int
 		idlModels[i] = &model.IDL{
 			Id:           idl.ID,
 			RepositoryId: idl.RepositoryID,
-			IdlType:      idl.IdlType,
 			MainIdlPath:  idl.IdlPath,
 			CommitHash:   idl.CommitHash,
 			ImportIdls:   nil,
