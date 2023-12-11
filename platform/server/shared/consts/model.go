dec1c25078c8451b19bbbf5d5680384688013f90
kinggo <lilong.21@bytedance.com>
2023-12-11 15:07:31 +0800

    chore: modify licenses header

--- platform/server/shared/consts/model.go
+++
@@ -1,9 +0,0 @@
-package consts
-
-const (
-	TableNameUser         = "user"
-	TableNameIDL          = "idl"
-	TableNameRepository   = "repository"
-	TableNameTemplate     = "template"
-	TableNameTemplateItem = "template_item"
-)
