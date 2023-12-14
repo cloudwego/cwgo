9a58e9cc833d73f9144e02c96b955b3693113e7b
cheerioInf <lltxsbsbg@gmail.com>
2023-12-14 10:49:18 +0800

    fix

--- platform/web/src/pages/TokenInfoPage/index.tsx
+++ platform/web/src/pages/TokenInfoPage/index.tsx
@@ -270,6 +270,7 @@ export default function TokenPage() {
 							style={{ width: 180 }}
 						/>
 						<Button
+							htmlType="submit"
 							onClick={() => {
 								const toast = Toast.info({
 									content: "正在搜索仓库",
